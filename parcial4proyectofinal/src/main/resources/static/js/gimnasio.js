9a402c78a974d145e3e95da347b904ba98c9cf3f
andreacuellar2 <cuellarandrea10@gmail.com>
2017-12-01 15:12:16 -0600

    cerrarsesion

--- parcial4proyectofinal/src/main/resources/static/js/gimnasio.js
+++ parcial4proyectofinal/src/main/resources/static/js/gimnasio.js
@@ -206,4 +206,8 @@ function login() {
                 }
                 location.reload();
             });
+    function cerrarsesion() {
+    	document.cookie="usuario=;expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    	location.href="/";
+    }
 }
